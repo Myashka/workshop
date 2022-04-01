e32d6b00561316c90a45dff122a0566e994a5f8c
Alexey Gorbatovskiy <porsh-5@yandex.ru>
2022-04-02 01:18:19 +0300

    Added contasts section

--- templates/js/onscroll.js
+++ templates/js/onscroll.js
@@ -1,16 +1,15 @@
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      $("#header").addClass("active");
-      $("#logo-svg .cls-1").css("stroke", "#000");
-      $(".logo a").addClass("active");
-      $(".navbar").removeClass("navbar-dark");
-      $(".navbar").addClass("navbar-light");
-    } else {
-      $("#header").removeClass("active");
-      $("#logo-svg .cls-1").css("stroke", "#fff");
-      $(".logo a").removeClass("active");
-      $(".navbar").removeClass("navbar-light");
-      $(".navbar").addClass("navbar-dark");
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    $("#header").addClass("active");
+    $("#logo-svg .cls-1").css("stroke", "#000");
+    $(".logo a").addClass("active");
+    $(".navbar").removeClass("navbar-dark");
+    $(".navbar").addClass("navbar-light");
+  } else {
+    $("#header").removeClass("active");
+    $("#logo-svg .cls-1").css("stroke", "#fff");
+    $(".logo a").removeClass("active");
+    $(".navbar").removeClass("navbar-light");
+    $(".navbar").addClass("navbar-dark");
   }
-  
+}
